dde0baad5fcf49da9f09587ea59dfe60f9c50c7d
Daniel Chen <chendaniely@users.noreply.github.com>
2025-01-22 22:29:49 -0800

    feat: add matplotlib

--- sandbox.py
+++ sandbox.py
@@ -1,2 +1,8 @@
-from pycounts_524_2024.pycounts_524_2024 import count_words
+from pycounts_524_2024.pycounts import count_words
 count_words("zen.txt")
+
+from pycounts_524_2024.plotting import plot_words
+
+counts = count_words("zen.txt")
+fig = plot_words(counts, 10)
+plt.show()
